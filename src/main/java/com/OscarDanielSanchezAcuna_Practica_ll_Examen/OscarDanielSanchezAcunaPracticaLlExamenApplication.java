e9627c610d32207ed4aca9df85838214845bc511
FreeCoffeeForAll <lightningsound@outlook.com>
2022-08-11 20:09:22 -0600

    Se crea practica ll examen

---
+++ src/main/java/com/OscarDanielSanchezAcuna_Practica_ll_Examen/OscarDanielSanchezAcunaPracticaLlExamenApplication.java
@@ -0,0 +1,13 @@
+package com.OscarDanielSanchezAcuna_Practica_ll_Examen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OscarDanielSanchezAcunaPracticaLlExamenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OscarDanielSanchezAcunaPracticaLlExamenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/OscarDanielSanchezAcuna_Practica_ll_Examen/OscarDanielSanchezAcunaPracticaLlExamenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.OscarDanielSanchezAcuna_Practica_ll_Examen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OscarDanielSanchezAcunaPracticaLlExamenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
